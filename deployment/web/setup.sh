f9564237a33ced19b5120a5fd48c21eb8f233f89
Vasco Ramos <vascoalramos@outlook.pt>
2020-12-15 17:59:01 +0000

    Update setup.sh

--- deployment/web/setup.sh
+++ deployment/web/setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && apt install -y nginx
 
-curl -o wiki https://raw.githubusercontent.com/vascoalramos/wiki-deployment/main/deployment/web/wiki?token=AJGNZAEOT3XWJP2HEPHQUE274IGFI
+curl -o wiki https://gist.githubusercontent.com/vascoalramos/724966925cb2e17b370dc12e77e1cd17/raw/698da8bdc8680892b2e1930def192eba3748a334/wiki.nginx
 mv wiki /etc/nginx/sites-available/default
 
 systemctl enable nginx
